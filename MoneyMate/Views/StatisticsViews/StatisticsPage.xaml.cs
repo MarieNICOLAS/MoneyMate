3d174be42c4a83b2b6507c040028207196adadb5
fatma <itchirfatma@gmail.com>
2026-03-17 17:20:00 +0100

    feat: add StatisticsPage with line and bar charts

--- MoneyMate/Views/StatisticsViews/StatisticsPage.xaml.cs
+++ MoneyMate/Views/StatisticsViews/StatisticsPage.xaml.cs
@@ -1,9 +1,13 @@
+using MoneyMate.Services;
+using MoneyMate.ViewModels;
+
 namespace MoneyMate.Views;
 
 public partial class StatisticsPage : ContentPage
 {
-	public StatisticsPage()
-	{
-		InitializeComponent();
-	}
+    public StatisticsPage(ExpenseService expenseService, BudgetService budgetService)
+    {
+        InitializeComponent();
+        BindingContext = new StatisticsViewModel(expenseService, budgetService);
+    }
 }
